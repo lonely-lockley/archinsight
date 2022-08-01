90ecf83826b43195842c5d166c73eb91095bcc3c
Oleg Konev <i.m.n-d@yandex.com>
2022-08-02 02:51:41 +0500

    Touchpad events was added (move/resize)

--- editor-ui/src/app/hooks/useSVGMove.ts
+++ editor-ui/src/app/hooks/useSVGMove.ts
@@ -1,5 +1,10 @@
 import React, { MutableRefObject, useCallback, useState } from 'react';
 
+type MoveEvent = {
+  x1: number;
+  y1: number;
+};
+
 /** SVG moving and scaling */
 export const useSVGMove = (svg: MutableRefObject<SVGElement | undefined>) => {
   const [scale, setScale] = useState(1);
@@ -15,33 +20,53 @@ export const useSVGMove = (svg: MutableRefObject<SVGElement | undefined>) => {
 
   const onMove = useCallback(
     (event: React.MouseEvent) => {
-      if (!isDraw || !svg.current) return;
-      event.stopPropagation();
+      if (!isDraw) return;
       event.preventDefault();
+      setViewBox({ x1: event.movementX, y1: event.movementY });
+    },
+    [isDraw],
+  );
 
+  const onWheel = useCallback(
+    (event: React.WheelEvent<HTMLDivElement>) => {
+      const native = event.nativeEvent as any;
+      const wdY = native?.wheelDeltaY || 0;
+      console.log(wdY, event.deltaY, event.deltaMode);
+      if (native.deltaX) {
+        setViewBox({ x1: -native.deltaX, y1: 0 });
+        return;
+      }
+      if (Math.abs(wdY) === 120) {
+        const factor = event.deltaY % 1 === 0 ? 0.5 : Math.abs(event.deltaY) / 10;
+        const newScale = event.deltaY < 0 ? scale + factor : scale - factor;
+        if (newScale <= 0.5) return;
+        setTransform(newScale);
+        return;
+      }
+      setViewBox({ x1: 0, y1: -native.deltaY });
+    },
+    [scale],
+  );
+
+  const setViewBox = useCallback(
+    ({ x1, y1 }: MoveEvent) => {
+      if (!svg.current) return;
       const viewBox = svg.current.getAttribute('viewBox');
       if (!viewBox) return;
 
       const [x, y, w, h] = viewBox.split(' ').map((a) => Number(a));
-      svg.current.setAttribute(
-        'viewBox',
-        `${x - event.movementX} ${y - event.movementY} ${w} ${h}`,
-      );
+      svg.current.setAttribute('viewBox', `${x - x1} ${y - y1} ${w} ${h}`);
     },
-    [isDraw, svg],
+    [svg],
   );
 
-  const onWheel = useCallback(
-    ({ deltaY }: React.WheelEvent<HTMLDivElement>) => {
+  const setTransform = useCallback(
+    (newScale: number) => {
       if (!svg.current) return;
-
-      const newScale = deltaY < 0 ? scale + 0.5 : scale - 0.5;
-      if (newScale <= 0.5) return;
-
       svg.current.setAttribute('transform', `scale(${newScale})`);
       setScale(newScale);
     },
-    [scale, svg],
+    [svg],
   );
 
   return { onDown, onUp, onMove, onWheel };
