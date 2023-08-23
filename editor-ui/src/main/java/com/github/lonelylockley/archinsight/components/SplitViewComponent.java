85cfe2739183bc41fbcd620bbc5469df7808ea2b
lonely-lockley <lonelylockley@gmail.com>
2023-08-23 13:05:38 +0300

    Added splitter position save

--- editor-ui/src/main/java/com/github/lonelylockley/archinsight/components/SplitViewComponent.java
+++ editor-ui/src/main/java/com/github/lonelylockley/archinsight/components/SplitViewComponent.java
@@ -1,10 +1,18 @@
 package com.github.lonelylockley.archinsight.components;
 
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.HtmlComponent;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.splitlayout.SplitLayout;
+import elemental.json.JsonType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class SplitViewComponent extends SplitLayout {
+public class SplitViewComponent extends PatchedSplitView {
+
+    private static final Logger logger = LoggerFactory.getLogger(SplitViewComponent.class);
+    private static final int defaultSplitterPosition = 40;
 
     public SplitViewComponent(HtmlComponent left, HtmlComponent right) {
         super();
@@ -15,6 +23,30 @@ public class SplitViewComponent extends SplitLayout {
         addToSecondary(svgBackground);
         setOrientation(Orientation.HORIZONTAL);
         setSizeFull();
-        setSplitterPosition(40);
+        getSavedSplitterPosition();
+        // initialize fixes introduced in PatchedSplitView
+        init();
+
+        addSplitterDragendListener(new ComponentEventListener<SplitterDragendEvent<SplitLayout>>() {
+            @Override
+            public void onComponentEvent(SplitterDragendEvent<SplitLayout> event) {
+                saveSplitterPosition();
+            }
+        });
+    }
+
+    private void getSavedSplitterPosition() {
+        getElement().executeJs("return localStorage.getItem('org.archinsight.splitter.pos')").then(value -> {
+            if (value.getType() == JsonType.NULL) {
+                setSplitterPosition(defaultSplitterPosition);
+            }
+            else {
+                setSplitterPosition(value.asNumber());
+            }
+        });
+    }
+
+    private void saveSplitterPosition() {
+        getElement().executeJs("return localStorage.setItem('org.archinsight.splitter.pos', $0)", getSplitterPosition());
     }
 }
