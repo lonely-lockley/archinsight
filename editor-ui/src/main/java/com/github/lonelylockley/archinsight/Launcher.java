521dd2ba180990f73a2fcd35d74df3a2bb532346
lonely-lockley <lonelylockley@gmail.com>
2023-08-24 10:05:58 +0300

    Added access logging

--- editor-ui/src/main/java/com/github/lonelylockley/archinsight/Launcher.java
+++ editor-ui/src/main/java/com/github/lonelylockley/archinsight/Launcher.java
@@ -6,7 +6,9 @@ import com.vaadin.flow.server.startup.ServletDeployer;
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
 import org.eclipse.jetty.plus.webapp.EnvConfiguration;
 import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.server.CustomRequestLog;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Slf4jRequestLogWriter;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.util.resource.ResourceCollection;
 import org.eclipse.jetty.webapp.*;
@@ -16,6 +18,8 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextEvent;
 import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,7 +35,18 @@ public final class Launcher {
         logger.info("Starting embedded Jetty server...");
         Server server = new Server(8080);
 
-        // Creation of a temporal directory
+        var tempDir = setupTemporaryDirectory();
+
+        var context = setupContext(server, tempDir);
+
+        setupAccessLogs(server);
+
+        server.start();
+        logger.info("Server started at port 8080");
+        server.join();
+    }
+
+    private static File setupTemporaryDirectory() {
         File tempDir = new File(System.getProperty("java.io.tmpdir"), "JettyTest");
         if (tempDir.exists()) {
             if (!tempDir.isDirectory()) {
@@ -40,7 +55,10 @@ public final class Launcher {
         } else if (!tempDir.mkdirs()) {
             throw new RuntimeException("Could not make: " + tempDir);
         }
+        return tempDir;
+    }
 
+    private static WebAppContext setupContext(Server server, File tempDir) throws IOException {
         WebAppContext context = new WebAppContext();
         context.setInitParameter("productionMode", "true");
 
@@ -76,8 +94,14 @@ public final class Launcher {
         // The base resource is where jetty serves its static content from
         context.setExtraClasspath(resourceList);
 
-        server.start();
-        logger.info("Server started at port 8080");
-        server.join();
+        return context;
+    }
+
+    private static void setupAccessLogs(Server server) {
+        var slfjRequestLogWriter = new Slf4jRequestLogWriter();
+        slfjRequestLogWriter.setLoggerName("AccessLog");
+        String format = "%{client}a - %u %t '%r' %s %O '%{Referer}i' '%{User-Agent}i' '%C'";
+        var requestLog = new CustomRequestLog(slfjRequestLogWriter, format);
+        server.setRequestLog(requestLog);
     }
 }
