5f319942d6b042b486113183e20956fde49dac41
lonely-lockley <lonelylockley@gmail.com>
2025-01-26 13:47:20 +0200

    Added some fixes, built new 2.0.0 version

--- editor-ui/src/main/java/com/github/lonelylockley/archinsight/screens/EditorView.java
+++ editor-ui/src/main/java/com/github/lonelylockley/archinsight/screens/EditorView.java
@@ -1,7 +1,5 @@
 package com.github.lonelylockley.archinsight.screens;
 
-import com.github.lonelylockley.archinsight.components.NotificationComponent;
-import com.github.lonelylockley.archinsight.model.remote.translator.MessageLevel;
 import com.github.lonelylockley.archinsight.security.Authentication;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.router.PageTitle;
@@ -16,11 +14,5 @@ public class EditorView extends BasicEditorView {
     public EditorView() {
         Authentication.disablePlaygroundMode();
         initView(null, false, 137);
-        new NotificationComponent("""
-                <div>
-                Breaking changes in the Insight language are planned for version 2.0.
-                You may read what's going to change and why in the <a target="_blank" href="https://medium.com/@lonelylockley/archinsight-the-evolution-from-architectural-diagrams-to-true-modeling-9bd70b6efae2">article on medium.com</a>.
-                </div>
-                """, MessageLevel.WARNING);
     }
 }
--- editor-ui/src/main/java/com/github/lonelylockley/archinsight/screens/PlaygroundView.java
+++ editor-ui/src/main/java/com/github/lonelylockley/archinsight/screens/PlaygroundView.java
@@ -1,11 +1,5 @@
 package com.github.lonelylockley.archinsight.screens;
 
-<<<<<<< HEAD
-import com.github.lonelylockley.archinsight.components.NotificationComponent;
-import com.github.lonelylockley.archinsight.model.remote.translator.MessageLevel;
-=======
-import com.github.lonelylockley.archinsight.MicronautContext;
->>>>>>> ab78c16 (Added external authentication for site integration. Still needs some polishing)
 import com.github.lonelylockley.archinsight.security.Authentication;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.router.PageTitle;
@@ -20,11 +14,5 @@ public class PlaygroundView extends BasicEditorView {
     public PlaygroundView() {
         Authentication.enablePlaygroundMode();
         initView("playground", true, 47);
-        new NotificationComponent("""
-                <div>
-                Breaking changes in the Insight language are planned for version 2.0.
-                You may read what's going to change and why in the <a target="_blank" href="https://medium.com/@lonelylockley/archinsight-the-evolution-from-architectural-diagrams-to-true-modeling-9bd70b6efae2">article on medium.com</a>.
-                </div>
-                """, MessageLevel.WARNING);
     }
 }
